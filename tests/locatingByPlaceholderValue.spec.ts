587f36fc5f10fbc37bd8e1ac5f48f98757fb1037
Dumpling <dumplingone@proton.me>
2026-08-26 22:20:33 -0600

    new file added for the Locating by placeholder value

--- tests/locatingByPlaceholderValue.spec.ts
+++ tests/locatingByPlaceholderValue.spec.ts
@@ -3,12 +3,12 @@ import { test, expect } from '@playwright/test';
 test('locating by placeholder value (TypeScript)', async ({ page }) => {
     // Step 1: Navigate to 'https://playwrightpad.com/practice/text-inputs/'.
     // TODO: Write your code here
-    await page.
+    await page.goto('https://playwrightpad.com/practice/text-inputs/')
     // Step 2: Locate input: page.getByPlaceholder('Enter username').
     // TODO: Write your code here
-
+    const usernameInput =  page.getByPlaceholder('e.g. playwright_master');
     // Step 3: Fill credentials.
     // TODO: Write your code here
+    await usernameInput.fill('qa-admin');
 
 });
-
