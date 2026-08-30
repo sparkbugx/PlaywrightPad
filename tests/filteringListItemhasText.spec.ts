14597eda3f7589fbdbecf1b0a10a64e5d043a7b4
Dumpling <dumplingone@proton.me>
2026-08-29 21:39:39 -0600

    The New two files to work are the locator Chaining Basics and the filltering List Item that has text

---
+++ tests/filteringListItemhasText.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from '@playwright/test';
+
+test('filtering list items hastext (TypeScript)', async ({ page }) => {
+    // Step 1: Navigate to 'https://playwrightpad.com/practice/filters-chaining/'.
+    // TODO: Write your code here
+
+    // Step 2: Filter list items: page.locator('li.stat-item').filter({ hasText: 'Active Users' }).
+    // TODO: Write your code here
+
+    // Step 3: Assert item count.
+    // TODO: Write your code here
+
+});
